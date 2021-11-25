58d3ce5aab8d7dda35c9da98aae3c3b168a01642
Ikarus Cesaille <mail.seadc@gmail.com>
2021-11-25 18:38:26 +0200

    child function

--- buybackprogram/forms.py
+++ buybackprogram/forms.py
@@ -14,21 +14,29 @@ class ProgramForm(forms.ModelForm):
 class ProgramItemForm(forms.ModelForm):
     marketgroups = EveMarketGroup.objects.filter(parent_market_group_id__isnull=True)
 
-    for m in marketgroups:
-        print("market group")
-        print(m)
+    def get_market_children(parent):
+        for children in parent:
+            return children.market_group_children.all()
 
-        m.child_groups = m.market_group_children.all()
+    for l1 in marketgroups:
 
-        for c in m.child_groups:
-            print("child")
-            print(c)
-            c.child_groups = c.market_group_children.all()
+        l1.child_groups = l1.market_group_children.all()
 
-            for d in c.child_groups:
-                print("sub")
-                print(d)
-                d.child_groups = d.market_group_children.all()
+        for l2 in l1.child_groups:
+
+            l2.child_groups = l2.market_group_children.all()
+
+            for l3 in l2.child_groups:
+
+                l3.child_groups = l3.market_group_children.all()
+
+                for l4 in l3.child_groups:
+
+                    l4.child_groups = l4.market_group_children.all()
+
+                    for l5 in l4.child_groups:
+
+                        l5.child_groups = l5.market_group_children.all()
 
     class Meta:
         model = ProgramItem

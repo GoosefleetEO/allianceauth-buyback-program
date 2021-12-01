efc49a5c89826dc6348bb0cd9f784d42317e7c8c
Ikarus Cesaille <pauli__parkkinen@hotmail.com>
2021-12-01 19:55:23 +1000

    price update task

--- buybackprogram/tasks.py
+++ buybackprogram/tasks.py
@@ -1,10 +1,8 @@
-from datetime import date
-
 import requests
 from celery import shared_task
 
 from django.db import Error
-from eveuniverse.models import EveType
+from django.utils import timezone
 
 from allianceauth.services.hooks import get_extension_logger
 
@@ -18,11 +16,16 @@ def update_all_prices():
 
     i = 0
     type_ids = []
+    market_data = {}
 
-    typeids = EveType.objects.values_list("id", flat=True).filter(published=True)
+    # Get all type ids
+    prices = ItemPrices.objects.all()
 
-    for item in typeids:
-        type_ids.append(item)
+    logger.debug("Price setup starting for %s items from Fuzzworks API" % len(prices))
+
+    # Build suitable bulks to fetch prices from API
+    for item in prices:
+        type_ids.append(item.eve_type_id)
 
         i += 1
 
@@ -37,30 +40,38 @@ def update_all_prices():
             )
 
             items_fuzzwork = response_fuzzwork.json()
-
-            for key, i in items_fuzzwork.items():
-
-                objs = [
-                    ItemPrices(
-                        id=key,
-                        eve_type_id=key,
-                        buy=int(float(i["buy"]["max"])),
-                        sell=int(float(i["sell"]["min"])),
-                        updated=date.today(),
-                    )
-                ]
-
-            try:
-                ItemPrices.objects.bulk_update(
-                    objs, ["buy", "sell", "updated", "eve_type_id"]
-                )
-            except Error as e:
-                logger.debug("Error with bulk create: %s" % e)
+            market_data.update(items_fuzzwork)
 
             i = 0
             type_ids.clear()
 
-    return "Updated prices for buybackprogram"
+    # Get leftover data from the bulk
+    response_fuzzwork = requests.get(
+        "https://market.fuzzwork.co.uk/aggregates/",
+        params={
+            "types": ",".join([str(x) for x in type_ids]),
+            "station": 60003760,
+        },
+    )
+
+    items_fuzzwork = response_fuzzwork.json()
+    market_data.update(items_fuzzwork)
+
+    logger.debug("Market data fetched, starting database update...")
+    for price in prices:
+
+        buy = int(float(market_data[str(price.eve_type_id)]["buy"]["max"]))
+        sell = int(float(market_data[str(price.eve_type_id)]["sell"]["min"]))
+
+        price.buy = buy
+        price.sell = sell
+        price.updated = timezone.now()
+
+    try:
+        ItemPrices.objects.bulk_update(prices, ["buy", "sell", "updated"])
+        logger.debug("All prices succesfully updated")
+    except Error as e:
+        logger.errro("Error updating prices: %s" % e)
 
 
 # from buybackprogram.tasks import update_all_prices

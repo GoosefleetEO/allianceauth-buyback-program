f5c0db9036b7a2674f7d7840b5863ad7193b3ffb
Erik Kalkoken <erik.kalkoken@gmail.com>
2023-04-06 20:49:05 +0200

    Improve data migration for cleaning up contracts

--- buybackprogram/tests/test_data_migrations.py
+++ buybackprogram/tests/test_data_migrations.py
@@ -5,7 +5,7 @@ from buybackprogram.models import Contract
 from django.apps import apps
 from django.db import connection
 from django.test import TestCase
-
+from app_utils.testing import set_test_logger
 from .testdata.factories import ContractFactory, ProgramFactory, TrackingFactory
 from .testdata.load_eveuniverse import load_eveuniverse
 
@@ -13,8 +13,14 @@ data_migration = import_module(
     "buybackprogram.migrations.0011_delete_duplicated_contracts"
 )
 
+set_test_logger(data_migration.logger.name, __file__)
 
-@unittest.skip  # Test no longer works after migration 0012 is applied
+
+def all_contracts() -> set:
+    return set(Contract.objects.all())
+
+
+# @unittest.skip  # Test no longer works after migration 0012 is applied
 class TestDataMigration(TestCase):
     @classmethod
     def setUpClass(cls) -> None:
@@ -28,28 +34,66 @@ class TestDataMigration(TestCase):
         # when
         data_migration.forwards(apps, connection.schema_editor())
         # then
-        self.assertIn(contract, Contract.objects.all())
+        self.assertSetEqual(all_contracts(), {contract})
 
-    def test_should_delete_duplicates(self):
+    def test_should_cleanup_variant_1(self):
         # given
         contract_1 = ContractFactory(contract_id=1)
         contract_2a = ContractFactory(contract_id=2, no_tracking=False)
         contract_2b = ContractFactory(contract_id=2, no_tracking=False)
+        ContractFactory(contract_id=2, no_tracking=False)
+        ContractFactory(contract_id=2, no_tracking=True)
         TrackingFactory(program=self.program, contract=contract_2a)
+        TrackingFactory(program=self.program, contract=contract_2b)
         # when
         data_migration.forwards(apps, connection.schema_editor())
         # then
-        self.assertIn(contract_1, Contract.objects.all())
-        self.assertIn(contract_2a, Contract.objects.all())
-        self.assertNotIn(contract_2b, Contract.objects.all())
+        self.assertSetEqual(all_contracts(), {contract_1, contract_2b})
 
-    def test_should_abort_when_duplicates_remain_after_deletion(self):
+    def test_should_cleanup_variant_2(self):
         # given
-        contract_1 = ContractFactory(contract_id=1)  # noqa: F841
-        contract_2a = ContractFactory(contract_id=2, no_tracking=False)
-        contract_2b = ContractFactory(contract_id=2, no_tracking=False)  # noqa: F841
-        contract_2c = ContractFactory(contract_id=2, no_tracking=True)  # noqa: F841
+        contract_1 = ContractFactory(contract_id=1)
+        ContractFactory(contract_id=2, no_tracking=True)
+        contract_2b = ContractFactory(contract_id=2, no_tracking=True)
+        contract_2c = ContractFactory(contract_id=2, no_tracking=True)
+        ContractFactory(contract_id=2, no_tracking=False)
+        TrackingFactory(program=self.program, contract=contract_2c)
+        # when
+        data_migration.forwards(apps, connection.schema_editor())
+        # then
+        self.assertSetEqual(all_contracts(), {contract_1, contract_2b})
+
+    def test_should_cleanup_variant_3(self):
+        # given
+        contract_1 = ContractFactory(contract_id=1)
+        ContractFactory(contract_id=2, no_tracking=False)
+        contract_2b = ContractFactory(contract_id=2, no_tracking=False)
+        contract_2c = ContractFactory(contract_id=2, no_tracking=True)
+        TrackingFactory(program=self.program, contract=contract_2c)
+        # when
+        data_migration.forwards(apps, connection.schema_editor())
+        # then
+        self.assertSetEqual(all_contracts(), {contract_1, contract_2b})
+
+    def test_should_cleanup_variant_4(self):
+        # given
+        contract_1 = ContractFactory(contract_id=1)
+        contract_2a = ContractFactory(contract_id=2, no_tracking=True)
+        contract_2b = ContractFactory(contract_id=2, no_tracking=True)
         TrackingFactory(program=self.program, contract=contract_2a)
-        # when/then
-        with self.assertRaises(SystemExit):
-            data_migration.forwards(apps, connection.schema_editor())
+        TrackingFactory(program=self.program, contract=contract_2b)
+        # when
+        data_migration.forwards(apps, connection.schema_editor())
+        # then
+        self.assertSetEqual(all_contracts(), {contract_1, contract_2b})
+
+    # def test_should_abort_when_duplicates_remain_after_deletion(self):
+    #     # given
+    #     contract_1 = ContractFactory(contract_id=1)  # noqa: F841
+    #     contract_2a = ContractFactory(contract_id=2, no_tracking=False)
+    #     contract_2b = ContractFactory(contract_id=2, no_tracking=False)  # noqa: F841
+    #     contract_2c = ContractFactory(contract_id=2, no_tracking=True)  # noqa: F841
+    #     TrackingFactory(program=self.program, contract=contract_2a)
+    #     # when/then
+    #     with self.assertRaises(SystemExit):
+    #         data_migration.forwards(apps, connection.schema_editor())
